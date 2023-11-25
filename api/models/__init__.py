f181ef10712180788742c587f104b4b67276ebb1
Fuad <fuadadio75@gmail.com>
2023-11-25 15:41:44 +0100

    feat: reordered imports in models init and made migrations

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -2,8 +2,8 @@ from api.models.base import TimeAndUUIDStampedBaseModel, SoftDeleteBaseModel
 from api.models.business import Business, BusinessTimeAndUUIDStampedBaseModel
 from api.models.user import User
 from api.models.team_invitiation import TeamInvitation
-from api.models.product import Product, ProductCategory, ProductItem, ProductSizeCategory
-from api.models.order import Order
-from api.models.vendor import Vendor
-from api.models.sales import Sales
 from api.models.customer import Customer
+from api.models.product import Product, ProductCategory, ProductItem, ProductSizeCategory
+from api.models.vendor import Vendor
+from api.models.order import Order
+from api.models.sales import Sales
