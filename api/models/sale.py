37adb3382f78c813e587abadf23baca954c9f929
Fuad <fuadadio75@gmail.com>
2023-11-26 17:31:42 +0100

    feat: worked on sales crud

--- api/models/sale.py
+++ api/models/sale.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import models, transaction
 from api.models import ProductItem, Customer, BusinessTimeAndUUIDStampedBaseModel
 
 class Sale(BusinessTimeAndUUIDStampedBaseModel):
@@ -8,13 +8,9 @@ class Sale(BusinessTimeAndUUIDStampedBaseModel):
     total_selling_price = models.DecimalField(max_digits=10, decimal_places=2, default= 0)
 
     @property
-    def sale_items(self) -> models.QuerySet:
+    def sale_items(self):
         return self.sale_items.all()
 
-    def save(self, *args, **kwargs):
-        self.total_selling_price = self.sale_items.aggregrate(total_sp=  models.Sum("selling_price"))["total_sp"]
-
-        super().save(*args, **kwargs)
     
     def __str__(self):
         return f"{self.id} - {self.created_at} - {self.customer.name}"
@@ -23,13 +19,23 @@ class Sale(BusinessTimeAndUUIDStampedBaseModel):
 class SaleItem(BusinessTimeAndUUIDStampedBaseModel):
     sale= models.ForeignKey(Sale, on_delete= models.CASCADE, related_name= "sale_items")
     product_item = models.ForeignKey(ProductItem, on_delete=models.CASCADE, related_name= "sales")
-    quantity = models.IntegerField(default= 1)
+    quantity = models.IntegerField()
     unit_selling_price = models.DecimalField(max_digits=10, decimal_places=2, default= 0)
     selling_price = models.DecimalField(max_digits=10, decimal_places=2)
     discount = models.DecimalField(max_digits=5, decimal_places=2, default=0)
 
+    @transaction.atomic
     def save(self, *args, **kwargs):
-        self.selling_price = self.quantity * self.unit_selling_price
+        if not self.pk:
+            self.product_item.quantity -= self.quantity
+            self.product_item.save()
+            
+
+        new_selling_price = self.quantity * self.unit_selling_price
+        if new_selling_price != self.selling_price:
+            self.sale.total_selling_price += new_selling_price - self.selling_price
+            self.selling_price= new_selling_price
+            self.sale.save()
 
         super().save(*args, **kwargs)
 
--- api/serializers/sale.py
+++ api/serializers/sale.py
@@ -1,9 +1,16 @@
+from django.db import transaction
 from rest_framework import serializers
 
-from api.models import Sale, SaleItem
+from api.models import Sale, SaleItem, ProductItem
 from api.serializers.product import ProductItemSerializer
 from api.serializers.customer import CustomerSerializer
 
+
+class CreateSaleItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SaleItem
+        fields = ["id", "product_item", "quantity"]
+        
 class SaleItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = SaleItem
@@ -23,14 +30,11 @@ class SaleItemSerializer(serializers.ModelSerializer):
         
 
 class SaleSerializer(serializers.ModelSerializer):
+    total_selling_price= serializers.DecimalField(max_digits=10, decimal_places=2, read_only= True)
+    sale_items= CreateSaleItemSerializer(many= True)
     class Meta:
         model = Sale
         fields = ["id", "customer", "description", "discount", "total_selling_price", "sale_items"]
-
-    
-    def create(self, validated_data):
-        validated_data["business"]= self.context["request"].user.business
-        return super().create(validated_data)
     
 
     def to_representation(self, instance: Sale):
@@ -40,7 +44,65 @@ class SaleSerializer(serializers.ModelSerializer):
             data["customer"]= CustomerSerializer(instance.customer).data
 
         if instance.sale_items.count():
-            data["sale_items"]= SaleItemSerializer(instance.sale_items.all(), many= True).data
-
+            data["sale_items"]= SaleItemSerializer(instance.sale_items, many= True).data
         
         return data
+    
+
+    def validate_sale_items(self, sale_items: list):
+        if not len(sale_items):
+            raise serializers.ValidationError("order must contain at least one sale_item")
+        
+        for sale_item in sale_items:
+            product_item: ProductItem= sale_item["product_item"]
+            quantity= sale_item["quantity"]
+
+            if product_item.quantity < quantity:
+                raise serializers.ValidationError(f"Not enough quantity of {product_item.name} available")
+
+
+
+        return sale_items
+    
+
+    @transaction.atomic
+    def create(self, validated_data):
+        business= self.context["request"].user.business
+        validated_data["business"]= business
+        sale_items= validated_data.pop("sale_items")
+        sale: Sale= super().create(validated_data)
+
+
+        total_selling_price= 0
+        new_sale_items= []
+        for sale_item in sale_items:
+            unit_selling_price= sale_item["product_item"].selling_price
+            selling_price= unit_selling_price * sale_item["quantity"]
+
+            sale_item= SaleItem(**{
+                **sale_item,
+                "sale": sale, 
+                "business": business, 
+                "unit_selling_price": unit_selling_price,
+                "selling_price": selling_price, 
+            })
+
+            new_sale_items.append(sale_item)
+            total_selling_price += selling_price
+
+
+        sale_items= SaleItem.objects.bulk_create(new_sale_items)
+        sale.total_selling_price= total_selling_price
+        sale.save()
+
+       
+        # update product_item quantity
+        product_items= []
+        for sale_item in sale_items:
+            sale_item.product_item.quantity -= sale_item.quantity
+            product_items.append(sale_item.product_item)
+
+        ProductItem.objects.bulk_update(product_items, ["quantity"])
+
+
+        return sale
