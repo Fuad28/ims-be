f11f2a5c24c8263476f6e3b9e477b74c055fead4
Fuad <fuadadio75@gmail.com>
2024-01-05 22:28:25 +0100

    feat: added category to Product model and made necessary updates in serializers

--- api/models/product.py
+++ api/models/product.py
@@ -1,16 +1,24 @@
 from django.db import models
 
-from api.models import BusinessTimeAndUUIDStampedBaseModel, Vendor, Category, SizeCategory
+from api.models import (
+    BusinessTimeAndUUIDStampedBaseModel,
+    Vendor,
+    Category,
+    SizeCategory,
+)
 from api.utils.utils import generate_serial_number
 
-null_blank= {"null": True, "blank": True}
+null_blank = {"null": True, "blank": True}
+
+
 class Product(BusinessTimeAndUUIDStampedBaseModel):
     name = models.CharField(max_length=255)
     image = models.URLField(**null_blank)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products", **null_blank)
 
     @property
     def quantity(self):
-        self.product_items.aggregate(total_sum= models.Sum("quantity"))["total_sum"]
+        self.product_items.aggregate(total_sum=models.Sum("quantity"))["total_sum"]
 
     def __str__(self):
         return f"{self.id} - {self.name} - {self.quantity}"
@@ -18,11 +26,19 @@ class Product(BusinessTimeAndUUIDStampedBaseModel):
 
 class ProductItem(BusinessTimeAndUUIDStampedBaseModel):
     name = models.CharField(max_length=255)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name= "product_items")
-    size_category = models.ForeignKey(SizeCategory, on_delete=models.CASCADE, related_name= "product_items")
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name= "product_items", **null_blank)
-    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, related_name= "product_items", **null_blank)
-    serial_no= models.CharField(max_length= 10)
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="product_items"
+    )
+    size_category = models.ForeignKey(
+        SizeCategory, on_delete=models.CASCADE, related_name="product_items"
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="product_items", **null_blank
+    )
+    vendor = models.ForeignKey(
+        Vendor, on_delete=models.CASCADE, related_name="product_items", **null_blank
+    )
+    serial_no = models.CharField(max_length=10)
     quantity = models.IntegerField(default=0)
     safety_stock = models.IntegerField(default=0)
     reordering_point = models.IntegerField(default=0)
@@ -34,18 +50,16 @@ class ProductItem(BusinessTimeAndUUIDStampedBaseModel):
     expiring_date = models.DateField(**null_blank)
     barcode = models.CharField(max_length=255)
     image = models.URLField(**null_blank)
-    
 
     def save(self, *args, **kwargs) -> None:
         if not self.serial_no:
-            count= ProductItem.objects.filter(business= self.business).count()
-            self.serial_no= generate_serial_number(count)
+            count = ProductItem.objects.filter(business=self.business).count()
+            self.serial_no = generate_serial_number(count)
 
         if not self.name:
-            self.name= f"{self.product.name} ({self.size_category.name})"
+            self.name = f"{self.product.name} ({self.size_category.name})"
 
         return super().save(*args, **kwargs)
-    
 
     def __str__(self):
         return f"{self.id} - {self.product.name} - {self.size_category.name}"
--- api/serializers/product.py
+++ api/serializers/product.py
@@ -1,52 +1,67 @@
 from rest_framework import serializers
 
-from api.models import Product, ProductItem
+from api.models import Product, ProductItem, Category
 from api.serializers.general import CategorySerializer, SizeCategorySerializer
 from api.serializers.vendor import SimpleVendorSerializer
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
 
     class Meta:
         model = Product
-        fields = ["id", "name", "image", "quantity"]
-
+        fields = ["id", "name", "category", "image", "quantity"]
 
     def create(self, validated_data):
-        validated_data["business"]= self.context["request"].user.business
+        validated_data["business"] = self.context["request"].user.business
         return super().create(validated_data)
 
+
 class ProductItemSerializer(serializers.ModelSerializer):
-    serial_no= serializers.CharField(max_length= 10, read_only= True)
+    serial_no = serializers.CharField(max_length=10, read_only=True)
+    category = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(), required=False
+    )
 
     class Meta:
         model = ProductItem
         fields = [
-            "id", "serial_no", "quantity", "safety_stock", "reordering_point",
-            "cost_price", "selling_price", "holding_cost", 
-            "ordering_cost", "expiring_date", "barcode", "image",
-            "product", "size_category", "category", "vendor"
+            "id",
+            "serial_no",
+            "quantity",
+            "safety_stock",
+            "reordering_point",
+            "cost_price",
+            "selling_price",
+            "holding_cost",
+            "ordering_cost",
+            "expiring_date",
+            "barcode",
+            "image",
+            "product",
+            "size_category",
+            "category",
+            "vendor",
         ]
 
     def to_representation(self, instance: ProductItem):
-        data=  super().to_representation(instance)
+        data = super().to_representation(instance)
 
         if instance.product:
-            data["product"]= ProductSerializer(instance.product).data
+            data["product"] = ProductSerializer(instance.product).data
 
         if instance.size_category:
-            data["size_category"]= SizeCategorySerializer(instance.size_category).data
+            data["size_category"] = SizeCategorySerializer(instance.size_category).data
 
         if instance.category:
-            data["category"]= CategorySerializer(instance.category).data
+            data["category"] = CategorySerializer(instance.category).data
 
-        
         if instance.vendor:
-            data["vendor"]= SimpleVendorSerializer(instance.vendor).data
+            data["vendor"] = SimpleVendorSerializer(instance.vendor).data
 
         return data
-    
 
     def create(self, validated_data):
-        validated_data["business"]= self.context["request"].user.business
+        validated_data["business"] = self.context["request"].user.business
+        validated_data["category"] = validated_data["product"].category
         return super().create(validated_data)
